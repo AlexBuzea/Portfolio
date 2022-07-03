541070c5717cf2ce4905bc4847ebee487b928a9d
AlexBuzea <60071473+AlexBuzea@users.noreply.github.com>
2022-07-03 16:53:31 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+(function ($) {
+    "use strict";
+  
+    //Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+})(jQuery);
+
+
+// hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
+// hamburger menu end
